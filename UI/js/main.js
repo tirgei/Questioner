3e25b3d3944d4e485ed73c78d4a5ab64b8423fce
Vincent Tirgei <tirgeic@gmail.com>
2019-01-28 16:05:00 +0300

    [Finishes #163498422] Add funtion to register new user

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,3 +1,48 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-alert */
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-disable indent */
+
+// Endpoints
+const BASE_URL = 'http://questioner-server.herokuapp.com/api/v2';
+const SIGNUP_URL = `${BASE_URL}/auth/signup`;
+
+/**
+ * Function to register a new user
+ */
+function register() {
+    fetch(SIGNUP_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            firstname: document.getElementById('firstname').value,
+            lastname: document.getElementById('lastname').value,
+            username: document.getElementById('username').value,
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value,
+            phonenumber: document.getElementById('phonenumber').value,
+        }),
+    })
+    .then(res => res.json())
+    .then((data) => {
+        if (data.status === 422) {
+            window.alert('Invalid data provided');
+        } else if (data.status === 201) {
+            window.location.replace('login.html');
+        } else {
+            window.alert(data.message);
+        }
+    })
+    .catch((error) => {
+        console.log(`Error: ${error}`);
+    });
+}
+
+
 function viewMeetup() {
     window.location = 'meetup.html';
 }
@@ -11,5 +56,5 @@ function createMeetup() {
 }
 
 function deleteMeetup() {
-    alert('Meetup deleted!')
+    alert('Meetup deleted!');
 }
